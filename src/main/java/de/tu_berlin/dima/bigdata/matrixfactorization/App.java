f89d6fe6ab20f196d4c58ce61a7a0f1846bc30f4
titicaca <fangzhou.sjtu@gmail.com>
2013-11-30 15:03:04 +0100

    init matrix factorization

---
+++ src/main/java/de/tu_berlin/dima/bigdata/matrixfactorization/App.java
@@ -0,0 +1,13 @@
+package de.tu_berlin.dima.bigdata.matrixfactorization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
